1bd9e7d0dab3002e640928d39b5a5c54af928d43
Eduardo Fischer <efischer@outlook.com.br>
2021-05-20 22:55:00 -0300

    Servidores em arquivo único e comentários

--- index.js
+++ index.js
@@ -4,25 +4,65 @@ const http = require('http')
 const https = require('https')
 const fs = require('fs')
 
-const app = express()
-
+// Definição das portas onde cada um dos servidores será servido
 const HTTP1_PORT = 8000
 const HTTPS1_PORT = 8001
 const HTTPS2_PORT = 8002
 
+// Criação da aplicação express - essa mesma aplicação será servida por cada um dos servidores abaixo
+const app = express()
+
+// Certificados SSL utilizados para a criptografia HTTPS
 const credentials = {
   key: fs.readFileSync(__dirname + '/server.key'),
   cert:  fs.readFileSync(__dirname + '/server.crt')
 }
 
+// Aqui definimos o diretório de onde serão servidos os arquivos estáticos
 app.use(express.static('public'))
 
+//ENDPOINTS - Abaixo são definidos cada um dos endpoints HTTP do experimento
 app.get('/', (req, res) => res.status(200).sendFile(__dirname + '/index.html'))
 app.get('/inline', (req, res) => res.status(200).sendFile(__dirname + '/index_inline.html'))
+app.get('/push', async (req, res) => {
+  try {
+    if(res.push){
+      [
+        "/style.css",
+        "/images/img1.jpeg",
+        "/images/img2.jpeg",
+        "/images/img3.jpeg",
+        "/images/img4.jpeg",
+        "/images/img5.jpeg",
+        "/images/img6.jpeg",
+        "/images/img7.jpeg",
+        "/images/img8.jpeg",
+        "/images/img9.jpeg",
+        "/images/img10.jpeg",
+        "/images/img11.jpeg",
+        "/images/img12.jpeg",
+        "/images/img13.jpeg",
+        "/images/img14.jpeg",
+        "/images/img15.jpeg",
+      ].forEach(async (file) => {
+        res.push(file, {}).end(await readFile(`public${file}`))
+      })
+    } 
+  } catch (e) {
+    res.status(500).send(error.toString())
+  }
+  
+  res.writeHead(200)
+  res.end(await readFile("index.html"))
+})
 
+// Aqui criamos os 3 servidores distintos (HTTP1.1, HTTPS1.1 e HTTPS2)
 const http1Server = http.createServer(app)
 const https1Server = https.createServer(credentials, app)
 const https2Server = spdy.createServer(credentials, app)
+
+// Abaixo, inicializamos cada servidor em sua respectiva porta
+// e imprimimos os endpoints disponíveis em cada um deles em caso de sucesso na inicialização
 
 http1Server.listen(HTTP1_PORT, (error) => {
   if (error) {
@@ -31,6 +71,7 @@ http1Server.listen(HTTP1_PORT, (error) => {
   } else {
     console.log('Servidor HTTP 1.1:')
     console.log(`- Página HTML + CSS + Imagens: http://localhost:${HTTP1_PORT}/`)
+    console.log(`- Página HTML + CSS + Imagens (Inlining): http://localhost:${HTTP1_PORT}/inline`)
     console.log(`- Texto 44Kb: http://localhost:${HTTP1_PORT}/small-text.txt`)
     console.log(`- Texto 2Mb: http://localhost:${HTTP1_PORT}/large-text.txt\n`)
   }
@@ -41,8 +82,9 @@ https1Server.listen(HTTPS1_PORT, (error) => {
     console.error(error)
     return process.exit(1)
   } else {
-    console.log('Servidor HTTPsS 1.1:')
+    console.log('Servidor HTTPS 1.1:')
     console.log(`- Página HTML + CSS + Imagens: http://localhost:${HTTPS1_PORT}/`)
+    console.log(`- Página HTML + CSS + Imagens (Inlining): http://localhost:${HTTPS1_PORT}/inline`)
     console.log(`- Texto 44Kb: http://localhost:${HTTPS1_PORT}/small-text.txt`)
     console.log(`- Texto 2Mb: http://localhost:${HTTPS1_PORT}/large-text.txt\n`)
   }
@@ -55,6 +97,8 @@ https2Server.listen(HTTPS2_PORT, (error) => {
   } else {
     console.log('Servidor HTTPS 2:')
     console.log(`- Página HTML + CSS + Imagens: http://localhost:${HTTPS2_PORT}/`)
+    console.log(`- Página HTML + CSS + Imagens (Server Push): http://localhost:${HTTPS2_PORT}/push`)
+    console.log(`- Página HTML + CSS + Imagens (Inlining): http://localhost:${HTTPS2_PORT}/inline`)
     console.log(`- Texto 44Kb: http://localhost:${HTTPS2_PORT}/small-text.txt`)
     console.log(`- Texto 2Mb: http://localhost:${HTTPS2_PORT}/large-text.txt\n`)
   }
